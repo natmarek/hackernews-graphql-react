cdcd0a1528c62249da122a81e5b124233b81d336
ripleycmd <53310862+ripleycmd@users.noreply.github.com>
2021-05-29 15:47:57 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import {
+	ApolloProvider,
+	ApolloClient,
+	createHttpLink,
+	InMemoryCache,
+} from "@apollo/client";
+
+const httpLink = createHttpLink({
+	uri: "http://localhost:4000",
+});
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
